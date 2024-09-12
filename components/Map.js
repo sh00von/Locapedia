fd74ce23d77599cb6fdb00c7374223266700b9ff
sh00von <u2110015@student.cuet.ac.bd>
2024-09-12 15:23:52 +0600

    Update

--- components/Map.js
+++ components/Map.js
@@ -7,6 +7,7 @@ import LoadingSpinner from './LoadingSpinner';
 import BottomSheet from './BottomSheet';
 import MapEvents from './MapEvents';
 import FitMapBounds from './FitMapBounds';
+import L from 'leaflet'; // Import Leaflet for custom icons
 import customIcon from './customIcon';
 import styles from './Map.module.css';
 import { useRouter } from 'next/router';
@@ -32,6 +33,7 @@ const Map = ({ locations, setLocations }) => {
   const [isAtUserLocation, setIsAtUserLocation] = useState(false);
   const [initialLoad, setInitialLoad] = useState(true); // Flag for initial load
   const [preventAutoCenter, setPreventAutoCenter] = useState(false); // State for preventing auto-centering
+  const [activeMarker, setActiveMarker] = useState(null); // State for active marker
 
   const router = useRouter();
   const { location: queryLocation } = router.query;
@@ -107,6 +109,12 @@ const Map = ({ locations, setLocations }) => {
     router.push('/', undefined, { shallow: true }); // Remove query parameter from URL but keep the center
   };
 
+  const handleMarkerClick = (location) => {
+    setSelectedLocation(location);
+    setActiveMarker(location); // Set the clicked marker as active
+    router.push(`/?location=${encodeURIComponent(location.title)}`, undefined, { shallow: true });
+  };
+
   const panToUserLocation = () => {
     if (userLocation) {
       setCenter(userLocation);
@@ -141,13 +149,14 @@ const Map = ({ locations, setLocations }) => {
           <Marker
             key={index}
             position={[location.lat, location.lon]}
-            icon={customIcon}
+            icon={activeMarker && activeMarker.title === location.title
+              ? L.divIcon({
+                  className: 'active-marker-icon',
+                  html: `<div class="${styles.activeMarker}"></div>`
+                })
+              : customIcon}
             eventHandlers={{
-              click: () => {
-                console.log('Marker clicked:', location); // Debugging line
-                setSelectedLocation(location);
-                router.push(`/?location=${encodeURIComponent(location.title)}`, undefined, { shallow: true });
-              },
+              click: () => handleMarkerClick(location),
             }}
           />
         ))}
@@ -168,8 +177,8 @@ const Map = ({ locations, setLocations }) => {
         onClick={panToUserLocation}
       >
         <svg width="32" height="32" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M13 3C13 2.44772 12.5523 2 12 2C11.4477 2 11 2.44772 11 3V4.06189C7.38128 4.51314 4.51314 7.38128 4.06189 11H3C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H4.06189C4.51314 16.6187 7.38128 19.4869 11 19.9381V21C11 21.5523 11.4477 22 12 22C12.5523 22 13 21.5523 13 21V19.9381C16.6187 19.4869 19.4869 16.6187 19.9381 13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H19.9381C19.4869 7.38128 16.6187 4.51314 13 4.06189V3ZM10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12ZM12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8Z" />
-</svg>
+          <path fillRule="evenodd" clipRule="evenodd" d="M13 3C13 2.44772 12.5523 2 12 2C11.4477 2 11 2.44772 11 3V4.06189C7.38128 4.51314 4.51314 7.38128 4.06189 11H3C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13H4.06189C4.51314 16.6187 7.38128 19.4869 11 19.9381V21C11 21.5523 11.4477 22 12 22C12.5523 22 13 21.5523 13 21V19.9381C16.6187 19.4869 19.4869 16.6187 19.9381 13H21C21.5523 13 22 12.5523 22 12C22 11.4477 21.5523 11 21 11H19.9381C19.4869 7.38128 16.6187 4.51314 13 4.06189V3ZM10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12ZM12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8Z" />
+        </svg>
       </div>
     </div>
   );
